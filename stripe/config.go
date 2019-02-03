8cda84f82154852480fce184d33082f4ed4b710f
Clinton Blackburn <clintonb@stripe.com>
2019-02-03 02:20:27 -0800

    Setting app info for user agent
    This plugin now identifies itself when communicating with Stripe, according to https://github.com/stripe/stripe-go/#writing-a-plugin.

--- stripe/config.go
+++ stripe/config.go
@@ -1,6 +1,7 @@
 package stripe
 
 import (
+	"github.com/stripe/stripe-go"
 	"log"
 
 	"github.com/stripe/stripe-go/client"
@@ -13,6 +14,10 @@ type Config struct {
 
 // Client returns a new Client for accessing Stripe.
 func (c *Config) Client() (*client.API, error) {
+	stripe.SetAppInfo(&stripe.AppInfo{
+		Name: "terraform-provider-stripe",
+	})
+
 	client := &client.API{}
 	client.Init(c.APIToken, nil)
 	log.Printf("[INFO] Stripe Client configured.")
